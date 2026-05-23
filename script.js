ae4763e7d4aeb121a02f1ed59cf88323d0009ba3
Muhammadimin0011-ship-it <muhademinmamateminov@gmail.com>
2026-05-23 15:08:17 +0500

    script.js

--- script.js
+++ script.js
@@ -1,15 +1,69 @@
-const API_KEY = "2c688a94a24cd6215037c4cce4825281"
-const API_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
 
 
-async function getMovies() {
-    const res = await fetch(
-        `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
-    );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const API_KEY = "2c688a94a24cd6215037c4cce4825281";
+
+async function getMovies(query) {
+
+    const url =
+        `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${query}`;
+
+    const res = await fetch(url);
+
     const data = await res.json();
 
-    console.log(data);
-    
+    console.log(data.results);
+
 }
 
-getMovies()
+getMovies("forsaj");
+
+
+
+
+
+
+
+//const API_KEY = "2c688a94a24cd6215037c4cce4825281"
+//const API_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+//
+//
+//async function getMovies() {
+//    const res = await fetch(
+//        `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+//    );
+//    const data = await res.json();
+//
+//    console.log(data);
+//
+//}
+//
+//getMovies()
+//
+//function changeLocationToDetailScren(id) {
+//    window.location.href = `/move/${id}`
+//}
